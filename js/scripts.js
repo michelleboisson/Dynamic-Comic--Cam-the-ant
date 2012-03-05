350aedf332fce03cd348b0852198ef915f4f8cb9
Michelle Boisson <michelle.boisson@gmail.com>
2012-03-04 19:30:14 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function getScrollXY() {
+  console.log("I'm triggered!");
+  var scrOfX = 0, scrOfY = 0;
+  if( typeof( window.pageYOffset ) == 'number' ) {
+    //Netscape compliant
+    scrOfY = window.pageYOffset;
+    scrOfX = window.pageXOffset;
+  } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+    //DOM compliant
+    scrOfY = document.body.scrollTop;
+    scrOfX = document.body.scrollLeft;
+  } else if( document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
+    //IE6 standards compliant mode
+    scrOfY = document.documentElement.scrollTop;
+    scrOfX = document.documentElement.scrollLeft;
+  }
+  //return [ scrOfX, scrOfY ];
+  return scrOfY;
+}
+
+
